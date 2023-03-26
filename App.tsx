ecfb5f3eea947eff4be3398f1f8a4ad1faf4bea9
Ariful Alam <alamariful1727@gmail.com>
2023-03-26 04:19:58 -0400

    feat: React Navigation added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text } from 'react-native';
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-2xl text-center">Typhoon On Demand</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
