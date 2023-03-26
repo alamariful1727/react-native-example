83fc7c8a2fdb28e70f71dfc06a70e27bfcc404fc
Ariful Alam <alamariful1727@gmail.com>
2023-03-26 04:19:44 -0400

    feat: tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}",],
+  content: ['./App.{js,jsx,ts,tsx}', './screens/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: { typGreen: '#3dcab1', typHeaderBlack: '#222222', typBodyBlack: '#292929', typDarkBlue: '#051229' },
+    },
   },
   plugins: [],
-}
+};
