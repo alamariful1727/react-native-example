1f51b1cf323b3ed61037414c776c20f0a4b05cad
Ariful Alam <alamariful1727@gmail.com>
2023-03-29 13:51:12 -0400

    feat: initial header design

--- screens/BrowseScreen.tsx
+++ screens/BrowseScreen.tsx
@@ -1,9 +1,16 @@
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, View, Image, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { MagnifyingGlassIcon } from 'react-native-heroicons/solid';
 
 const BrowseScreen = () => {
   return (
-    <SafeAreaView className="bg-typDarkBlue flex-1 justify-center">
+    <SafeAreaView className="bg-typDarkBlue flex-1">
+      <View className="px-5 py-3 flex-row justify-between">
+        <Image source={require('../assets/typhoon.png')} className="h-8 w-36" />
+        <TouchableOpacity>
+          <MagnifyingGlassIcon fill="white" size={28} />
+        </TouchableOpacity>
+      </View>
       <Text className="text-2xl text-center text-white">Typhoon On Demand</Text>
     </SafeAreaView>
   );
