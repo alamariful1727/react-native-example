3eebe746fc4a8292dc5d2dce76f81247f8e619ca
Ariful Alam <alamariful1727@gmail.com>
2023-04-02 14:08:46 -0400

    feat: react-query configured

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import LoginScreen from './screens/LoginScreen';
 import SearchScreen from './screens/SearchScreen';
 import SplashScreen from './screens/SplashScreen';
 import { RootStackParamList } from './types';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -13,21 +14,27 @@ const screenOptions = {
   headerShown: false,
 };
 
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: 2 } },
+});
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        style={{ flex: 1 }}
-        keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
-      >
-        <Stack.Navigator initialRouteName="Splash" screenOptions={screenOptions}>
-          <Stack.Screen name="Splash" component={SplashScreen} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Browse" component={BrowseScreen} />
-          <Stack.Screen name="Search" component={SearchScreen} />
-        </Stack.Navigator>
-      </KeyboardAvoidingView>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={{ flex: 1 }}
+          keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+        >
+          <Stack.Navigator initialRouteName="Splash" screenOptions={screenOptions}>
+            <Stack.Screen name="Splash" component={SplashScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Browse" component={BrowseScreen} />
+            <Stack.Screen name="Search" component={SearchScreen} />
+          </Stack.Navigator>
+        </KeyboardAvoidingView>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
